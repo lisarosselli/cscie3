e68b6c19790a171747514fcfa6cc7cad0b85985b
Lisa Rosselli <studio1809@gmail.com>
2015-02-21 12:47:46 -0600

    splitting name string

--- cscie3ProjectUnit2A/js/hw2SplitString.js
+++ cscie3ProjectUnit2A/js/hw2SplitString.js
@@ -14,33 +14,26 @@
   ********************************************************************/
 
 
-  var splitName = document.getElementById("splitName");
+var splitName = document.getElementById("splitName");
 splitName.onclick = function(){
 
                var fullname = document.getElementById("fullName");
-
-               /*
-                *  We've gotten the fullname from the HTML form field.
-                *  Your job is to use the String.slice(), String.substring() or String.substr() functions
-                *  to divide your name into separate first and last name strings and assign
-                *  them to the variables provided. You may
-                *  need String.indexOf() as well.
-                *
-                *  You may not hardcode the position of where to split the string. Your code should
-                *  work for anyone's first and last name. If the user enters a name without any whitespace
-                *  return that as firstname and leave lastname blank.
-                *
-                *  If a name has more than one whitespace (as in, using one or more middle names),
-                *  make the first name 'firstname' and assign the rest to 'lastname'
-                *
-                **/
-
-               var firstname = 'Placeholder first name';
-               var lastname = 'Placeholder last name';
-
+               var userName = fullname.value;
+               var regExp = /^[a-z A-Z]+(-[a-z A-Z]+)?$/;
+          
+               if (!regExp.test(userName)) {
+                 // Exit out of this function if the name contains
+                 // items that are not "name like" (e.g. numbers, 
+                 // or characters that are not letters).
+                   console.log("User entered something not like a name.");
+                   return;
+               }
+               
+               var spaceIndex = userName.indexOf(" ");
+               var firstname = userName.slice(0, spaceIndex);
+               var lastname = userName.slice(spaceIndex+1, userName.length);
 
                document.getElementById("firstname").innerHTML = firstname;
                document.getElementById("lastname").innerHTML = lastname;
-
 
 }
