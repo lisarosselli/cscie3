0b42b7bce46f5e12993683014939b4a10ee97c48
Lisa Rosselli <studio1809@gmail.com>
2015-02-20 20:53:11 -0600

    place the x

--- cscie3ProjectUnit2A/js/hw2PutAnX.js
+++ cscie3ProjectUnit2A/js/hw2PutAnX.js
@@ -17,28 +17,16 @@ var putAnXBtn = document.getElementById("putAnXButton");
 putAnXBtn.onclick = function(){
 
  	// get a reference to the box
-        var theBox = document.getElementById("putAnX");
+  var theBox = document.getElementById("putAnX");
+  var width = theBox.clientWidth;
+  var height = theBox.clientHeight;
 
-        // now get the width and height of the box - see the clientWidth and clientHeight documentation at http://docs.webplatform.org/wiki/dom/HTMLElement
-        var width = theBox.clientWidth;
-        var height = theBox.clientHeight;
-
-        /*
-         *   Now, here's where you do your magic. The xPosition and yPosition should not be set to zero.
-         *
-         *   Use the Math.random() function to get a number between
-         *   0 and 1, then use some math to convert that to a number between 0 and the width. This is your x position.
-         *
-         *   Do the same thing to generate a number between 0 and the height: this is your y position.
-         *   */
-
-        var yPosition = 0; // should generate a value between 0 and the height
-        var xPosition = 0;  //should generate a value between 0 and the width
-
+  var xPosition = Number.parseInt(Math.random() * width);
+  var yPosition = Number.parseInt(Math.random() * height);
+        
  	//now we'll get the HTML element where the X goes, fill in the X and set the position of the element.
  	var theXElement = document.getElementById("theX");
-        theXElement.innerHTML="X";
-        theXElement.style.top = parseInt(yPosition)+'px';
-        theXElement.style.left = parseInt(xPosition)+'px';
-
- }
+  theXElement.innerHTML="X";
+  theXElement.style.top = yPosition+'px';
+  theXElement.style.left = xPosition+'px';
+}
