f051b1667ee84a22e67b42ce134a797ccbe11b15
Lisa Rosselli <studio1809@gmail.com>
2015-04-06 21:54:33 -0500

    meager beginnings to 3B

--- cscie3ProjectUnit3B/js/hw3b.js
+++ cscie3ProjectUnit3B/js/hw3b.js
@@ -1,2 +1,25 @@
-/* hw3b.js */
+/* 
+ * Unit 3B
+ *
+ * @author L.Rosselli
+ *
+ */
+
+
+
+/*
+ * #1: Passwords
+ */
+
+var password1 = document.getElementById('pwd1');
+var password2 = document.getElementById('pwd2');
+debugger;
+
+function validatePasswords(e) {
+	console.log('validatePasswords');
+}
+
+password2.addEventListener('change', function(e) {
+	console.log('test');
+});
 
